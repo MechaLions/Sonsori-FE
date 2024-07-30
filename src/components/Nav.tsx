38c84a74b5747b3ce51c69120babfa8dae9b86de
SonSuBin129 <ttokky0129@gmail.com>
2024-07-31 01:36:51 +0900

    style: nav바 구현
    - 추후 색상 변경 필요

---
+++ src/components/Nav.tsx
@@ -0,0 +1,49 @@
+import { Link, useLocation } from "react-router-dom";
+import { cn } from "@ui/lib/utils";
+
+import UserIcon from "./Icons/UserIcon";
+import Logo from "./Icons/Logo";
+
+interface NavItemProps {
+  link: string;
+  title: string;
+}
+
+const NavItem = (props: NavItemProps) => {
+  const { link, title } = props;
+  const location = useLocation();
+
+  const isActive = location.pathname === link;
+
+  return (
+    <Link to={link} className="text-xl font-bold">
+      <p
+        className={cn(
+          "hover:text-blue-300",
+          isActive ? "text-blue-500" : "text-black",
+        )}
+      >
+        {title}
+      </p>
+    </Link>
+  );
+};
+
+const Nav = () => {
+  return (
+    <nav className="flex items-center justify-between px-[88px] py-3.5">
+      <Link to="/home">
+        <Logo />
+      </Link>
+      <NavItem title="수어 번역" link="/translate" />
+      <NavItem title="수어 쉐도잉" link="/shadowing" />
+      <NavItem title="퀴즈" link="/quiz" />
+      <NavItem title="발음 교정" link="/pronunciation" />
+      <Link to="/mypage">
+        <UserIcon />
+      </Link>
+    </nav>
+  );
+};
+
+export default Nav;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <main className="flex h-full flex-col"></main>;
+  return <main className="flex h-full flex-col">지금 home 화면</main>;
 };
 
 export default Home;
