9697b4132fc4a88b17019ec8ba8562f002e7d310
SonSuBin129 <ttokky0129@gmail.com>
2024-08-14 00:24:05 +0900

    style: 컴포넌트 수정

--- ui/src/components/ui/button.tsx
+++ ui/src/components/ui/button.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "@ui/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -18,9 +17,11 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        brand:
+          "text-white font-semibold text-base rounded-[20px]  bg-brand shadow-[0px_4px_4px_rgba(0,0,0,0.25)]",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "w-fit py-[15px] px-[38px]",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
