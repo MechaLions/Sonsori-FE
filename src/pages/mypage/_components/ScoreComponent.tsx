08db73446a53ff795751a8f5604dfba9f4eaee5f
SonSuBin129 <ttokky0129@gmail.com>
2024-08-21 17:22:26 +0900

    style: 기능별 학습 진행률 추가

---
+++ src/pages/mypage/_components/ScoreComponent.tsx
@@ -0,0 +1,11 @@
+const ScoreComponent = () => {
+  return (
+    <section className="mb-[60px] flex items-center justify-center text-[77px] font-semibold">
+      <p className="inline-block text-brand">8</p>
+      <p className="mt-[60px] inline-block rotate-[15deg]">/</p>
+      <p className="mt-[120px] inline-block">10</p>
+    </section>
+  );
+};
+
+export default ScoreComponent;
--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,5 +1,17 @@
+import ShadowingPart from "./_components/ShadowingPart";
+import QuizPart from "./_components/QuizPart";
+import PronunciationPart from "./_components/PronunciationPart";
+
 const MyPage = () => {
-  return <main className="flex h-full flex-col">지금 MyPage 화면</main>;
+  return (
+    <main className="flex h-full flex-col">
+      <section className="flex h-full w-full items-center justify-evenly">
+        <ShadowingPart />
+        <QuizPart />
+        <PronunciationPart />
+      </section>
+    </main>
+  );
 };
 
 export default MyPage;
