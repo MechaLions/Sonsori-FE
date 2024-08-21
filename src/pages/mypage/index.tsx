b3ca17283b3edc111dfb248927f1421a1b00b731
SonSuBin129 <ttokky0129@gmail.com>
2024-08-21 20:21:38 +0900

    style: 마이페이지 멘트 추가

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -2,21 +2,33 @@ import ShadowingPart from "./_components/ShadowingPart";
 import QuizPart from "./_components/QuizPart";
 import PronunciationPart from "./_components/PronunciationPart";
 
+import UserImage from "@/assets/UserImage.png";
+
 const MyPage = () => {
   return (
     <main
-      className="flex h-full flex-col"
+      className="h-full"
       style={{
         background:
           "linear-gradient(123deg, #7CABE9 54%, #0169F4 79%, #055ED6 99%)",
       }}
     >
-      <section></section>
-      <section className="flex h-full w-full items-center justify-evenly bg-white/[.42]">
-        <ShadowingPart />
-        <QuizPart />
-        <PronunciationPart />
-      </section>
+      <div className="flex h-full w-full flex-col justify-evenly bg-white/[.42]">
+        <section className="flex flex-col gap-5">
+          <div className="flex items-center justify-center">
+            <img src={UserImage} width={100} />
+          </div>
+          <div className="text-center text-[35px] font-bold lg:text-[40px]">
+            <p className="inline-block text-brand">손소리</p>
+            <p className="inline-block">님의 학습 공간</p>
+          </div>
+        </section>
+        <section className="flex w-full items-center justify-evenly">
+          <ShadowingPart />
+          <QuizPart />
+          <PronunciationPart />
+        </section>
+      </div>
     </main>
   );
 };
