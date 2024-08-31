00ed02aa700fdfde7a47683d609ed122754c0720
minikryc <mini122600@gmail.com>
2024-08-31 18:27:44 +0900

    style: 웹캠 연동 및 촬영 화면 구현

--- src/pages/translate/_components/TranslatePage.tsx
+++ src/pages/translate/_components/TranslatePage.tsx
@@ -1,49 +1,70 @@
+import Webcam from "react-webcam";
 import { useState, useRef, useEffect } from "react";
 import { Button } from "@ui/components/ui/button";
 
 import StartQuoteIcon from "@/components/Icons/StartQuoteIcon";
 import EndQuoteIcon from "@/components/Icons/EndQuoteIcon";
 
+const FACING_MODE_ENVIRONMENT = "environment";
+
+const videoConstraints = {
+  facingMode: FACING_MODE_ENVIRONMENT,
+  advanced: [
+    { width: { exact: 2560 }, height: { exact: 1440 } },
+    { width: { exact: 1920 }, height: { exact: 1080 } },
+    { width: { exact: 1280 }, height: { exact: 720 } },
+    { width: { exact: 1024 }, height: { exact: 576 } },
+    { width: { exact: 900 }, height: { exact: 506 } },
+    { width: { exact: 800 }, height: { exact: 450 } },
+    { width: { exact: 640 }, height: { exact: 360 } },
+    { width: { exact: 320 }, height: { exact: 180 } },
+  ],
+};
+
 const TranslatePage = () => {
   // 보여지는 버튼 set 제어
   const [showMainButtons, setShowMainButtons] = useState(true);
   const [isCameraOn, setIsCameraOn] = useState(false);
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const startCamera = () => setIsCameraOn(true);
+  const stopCamera = () => setIsCameraOn(false);
 
   // 카메라 켜기
-  const startCamera = async () => {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-          setIsCameraOn(true);
-        }
-      } catch (err) {
-        console.error("카메라 접근에 실패했습니다:", err);
-      }
-    }
-  };
-
-  // 카메라 끄기
-  const stopCamera = () => {
-    if (videoRef.current && videoRef.current.srcObject) {
-      const stream = videoRef.current.srcObject as MediaStream;
-      const tracks = stream.getTracks();
-      tracks.forEach(track => track.stop());
-      videoRef.current.srcObject = null;
-      setIsCameraOn(false);
-    }
-  };
-
   useEffect(() => {
-    // 컴포넌트가 언마운트될 때 카메라를 끄는 처리
-    return () => {
-      stopCamera();
+    const initCamera = async () => {
+      try {
+        if (isCameraOn) {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+          });
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+          }
+        } else {
+          if (videoRef.current && videoRef.current.srcObject) {
+            const tracks = (
+              videoRef.current.srcObject as MediaStream
+            ).getTracks();
+            tracks.forEach(track => track.stop());
+            videoRef.current.srcObject = null;
+          }
+        }
+      } catch (error) {
+        console.error("카메라 접근 실패:", error);
+      }
     };
-  }, []);
+
+    initCamera();
+
+    // 컴포넌트가 언마운트될 때 카메라 스트림 해제
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        const tracks = (videoRef.current.srcObject as MediaStream).getTracks();
+        tracks.forEach(track => track.stop());
+      }
+    };
+  }, [isCameraOn]);
 
   return (
     <main
@@ -110,7 +131,7 @@ const TranslatePage = () => {
       </div>
 
       {/* 카메라 화면이 들어갈 이미지 박스 */}
-      <div className="group relative mt-[30px] flex h-[510px] w-[766px] items-center justify-center rounded-2xl bg-white shadow-lg">
+      <div className="group relative mt-[30px] flex h-[510px] w-[766px] items-center justify-center rounded-2xl bg-white px-[34px] pb-10 shadow-lg">
         {/* 작은 점 세개 */}
         <div className="absolute top-[15px] flex space-x-1">
           <span className="h-2 w-2 rounded-full bg-grey"></span>
@@ -120,28 +141,34 @@ const TranslatePage = () => {
 
         {/* 카메라 영상 */}
         {isCameraOn ? (
-          <video
-            ref={videoRef}
-            autoPlay
-            className="h-full w-full rounded-lg"
-          ></video>
-        ) : (
-          <img
-            src="https://via.placeholder.com/698x428"
-            alt="placeholder"
-            className="rounded-lg"
+          <Webcam
+            id="main-webcam"
+            audio={false}
+            className="mt-[41px] rounded-2xl"
+            videoConstraints={videoConstraints}
+            style={{
+              width: "698px",
+              height: "428px",
+              objectFit: "cover", // 비디오가 부모 요소에 맞게 채워지도록 설정
+            }}
           />
+        ) : (
+          <div className="my-50 flex-center mt-10 flex h-[428px] w-[698px] justify-center rounded-2xl bg-[#434242]">
+            <h1 className="mt-[197px] text-center text-[28px] font-semibold text-[#D9D9D9]">
+              <p>시작하기를 누르면 번역을 위한 촬영이 시작됩니다. </p>
+            </h1>
+          </div>
         )}
       </div>
 
       {/* 텍스트 박스 */}
       <div className="group mt-[57px] flex h-[115px] w-[766px] items-center justify-center rounded-2xl border-[2px] border-brand bg-white p-4 text-center">
         {/* 텍스트 양옆 quotation icons */}
-        <StartQuoteIcon className="absolute left-[294px] mt-[-100px]" />
-        <p className="text-[20px] text-brand">
-          시작하기 버튼을 누르면 번역을 위한 촬영이 시작됩니다.
+        <StartQuoteIcon className="absolute mb-[100px] mr-[830px]" />
+        <p className="text-center text-[20px] text-brand">
+          번역되는 내용이 여기에 표시됩니다.
         </p>
-        <EndQuoteIcon className="absolute right-[294px] mt-[-100px]" />
+        <EndQuoteIcon className="absolute mb-[100px] ml-[830px]" />
       </div>
     </main>
   );
