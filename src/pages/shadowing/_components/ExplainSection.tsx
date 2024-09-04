b7a8a0ffea7f0f40aae7ccc14699dc86b065c1a0
minikryc <mini122600@gmail.com>
2024-09-04 20:18:31 +0900

    fix: 카테고리 설명 문구 수정

--- src/pages/shadowing/_components/ExplainSection.tsx
+++ src/pages/shadowing/_components/ExplainSection.tsx
@@ -2,14 +2,13 @@ const ExplainSection = () => {
   return (
     <section className="flex flex-col gap-4 text-center">
       <h1 className="text-[40px] font-bold leading-tight">
-        표준 발음 텍스트를 보고 녹음하며,
+        다양한 카테고리를 통해
         <br />
-        발음 정확도를 스마트하게 측정해보세요!
+        실행활에 유용한 수어를 학습해보세요!
       </h1>
-      <p className="text-2xl font-semibold text-brand">
-        텍스트를 통해 표준 발음을 쉽게 확인 가능
-        <br />
-        실시간 정확도 측정
+      <p className="flex justify-center gap-20 text-2xl font-semibold text-brand">
+        <span>따라하기를 통한 반복 학습</span>
+        <span>실시간 정확도 측정</span>
       </p>
     </section>
   );
