1f25b8ae7f25bc76f58d8de34439da575546bec1
minikryc <mini122600@gmail.com>
2024-09-04 20:44:32 +0900

    style: quiz activity 구현 및 결과확인 버튼 위치 조정

---
+++ src/pages/shadowing/_components/QuizActivity.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@ui/components/ui/button";
+import { ActivityComponentType } from "@stackflow/react";
+import { AppScreen } from "@stackflow/plugin-basic-ui";
+
+import ProgressBar from "@/components/ProgressBar";
+
+import { usePronunciationFlow } from "@/utils/usePronunciationFlow";
+
+import PropmptSection from "./PromptSection";
+import { Activity, ActivityHeader, ActivityContent } from "./Activity";
+
+type QuizParams = {
+  step: number;
+};
+
+const QuizActivity: ActivityComponentType<QuizParams> = ({ params }) => {
+  const { step } = params;
+
+  const { replace } = usePronunciationFlow();
+
+  const handleClick = () => {
+    replace(
+      "AnswerActivity",
+      {
+        step: step,
+      },
+      { animate: false },
+    );
+  };
+
+  return (
+    <AppScreen>
+      <Activity>
+        <ActivityContent>
+          <ActivityHeader step={step}>
+            <ProgressBar percent={step / 10} />
+            <div className="relative flex h-20 w-[70%] items-center">
+              <Button
+                variant="brand"
+                className="absolute right-[-150px] mb-[250px]"
+                onClick={handleClick}
+              >
+                결과 확인
+              </Button>
+            </div>
+          </ActivityHeader>
+          <div className="mt-[-50px] justify-evenly">
+            <PropmptSection />
+          </div>
+        </ActivityContent>
+      </Activity>
+    </AppScreen>
+  );
+};
+
+export default QuizActivity;
