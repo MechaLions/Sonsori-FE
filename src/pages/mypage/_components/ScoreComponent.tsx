5865b06df0063741f3b6f22082899daf76a34cfe
SonSuBin129 <ttokky0129@gmail.com>
2024-09-28 19:46:53 +0900

    feat: api 연결 이후 컴포넌트 props 추가
    - 학습 결과가 없는 경우 대신할 컴포넌트 생성

--- src/pages/mypage/_components/ScoreComponent.tsx
+++ src/pages/mypage/_components/ScoreComponent.tsx
@@ -1,7 +1,13 @@
-const ScoreComponent = () => {
+interface ScoreComponentProps {
+  correctNumber: number;
+}
+
+const ScoreComponent = (props: ScoreComponentProps) => {
+  const { correctNumber } = props;
+
   return (
     <section className="mb-8 flex items-center justify-center text-[77px] font-semibold">
-      <p className="inline-block text-brand">8</p>
+      <p className="inline-block text-brand">{correctNumber}</p>
       <p className="mt-10 inline-block rotate-[15deg]">/</p>
       <p className="mt-[80px] inline-block">10</p>
     </section>
