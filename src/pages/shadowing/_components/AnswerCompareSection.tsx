f09f6a4326151e5a8b15b04d636814a7c0419a63
minikryc <mini122600@gmail.com>
2024-10-12 20:35:14 +0900

    refactor: 변수명 통일

--- src/pages/shadowing/_components/AnswerCompareSection.tsx
+++ src/pages/shadowing/_components/AnswerCompareSection.tsx
@@ -1,6 +1,6 @@
 interface AnswerCompareSectionProps {
-  correctText: string;
-  userText: string | string[];
+  correct_text: string;
+  user_text: string | string[];
 }
 
 // 발음을 비교하여 같은 글자는 초록색, 다른 글자는 빨간색으로
@@ -22,17 +22,17 @@ const getColoredText = (correct: string, user: string | string[]) => {
 };
 
 const AnswerCompareSection = (props: AnswerCompareSectionProps) => {
-  const { correctText, userText } = props;
+  const { correct_text, user_text } = props;
   return (
     <div className="flex w-1/2 flex-col items-center justify-center gap-10 rounded-[20px] bg-brandLightBlue py-9 pl-[60px] leading-tight">
       <div className="flex w-full flex-col gap-1">
         <p className="text-base text-brandMediumGray">올바른 문장</p>
-        <p className="text-3xl font-semibold">{correctText}</p>
+        <p className="text-3xl font-semibold">{correct_text}</p>
       </div>
       <div className="flex w-full flex-col gap-1">
         <p className="text-base text-brandMediumGray">사용자 입력 문장</p>
         <p className="text-3xl font-semibold">
-          {getColoredText(correctText, userText)}
+          {getColoredText(correct_text, user_text)}
         </p>
       </div>
     </div>
